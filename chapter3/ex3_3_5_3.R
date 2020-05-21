1756a344ec82c677be8ace35d3428f68269263d0
michalk <michkam89@gmail.com>
2020-05-21 22:15:02 +0200

    finished chapter 3

---
+++ chapter3/ex3_3_5_3.R
@@ -0,0 +1,13 @@
+library(shiny)
+ui <- fluidPage(
+  dataTableOutput("table")
+)
+server <- function(input, output, session) {
+  output$table <- renderDataTable(mtcars, 
+                                  options = list(
+                                    pageLength = 10,
+                                    searching = FALSE,
+                                    ordering = FALSE
+                                  ))
+}
+shinyApp(ui, server)
